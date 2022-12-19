8c0c5043f1fede9d5dbbb523c0e8089817431003
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-12-19 13:04:01 +0530

    Added follow/unfollow routes

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Doctor = require("../models/Doctor");
+const Patient = require("../models/Patient");
+const { route } = require("./doctorAuth");
 
 router.get("/all", async (req, res) => {
     try {
@@ -21,8 +23,55 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+router.put("/:id/follow", async (req, res) => {
+    if (req.params.id != req.body.userId) {
+        try {
+            const doctor = await Doctor.findById(req.params.id);
+            const patient = await Patient.findById(req.body.userId);
+            if (!patient.following.includes(req.params.id)) {
+                await patient.updateOne({ $push: { following: req.params.id } });
+                await doctor.updateOne({ $push: { followers: req.body.userId } });
+
+            } else {
+                res.status(400).send("You already follow this doctor");
+            }
+            res.status(200).send("Doctor has been followed");
+        } catch (e) {
+            res.status(500).json(e);
+
+        }
+    } else {
+        res.status(401).send("You can't follow yourself");
+    }
+})
+
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.params.id != req.body.userId) {
+        try {
+            const doctor = await Doctor.findById(req.params.id);
+            const patient = await Patient.findById(req.body.userId);
+            console.log(doctor);
+            console.log(patient);
+            if (patient.following.includes(req.params.id)) {
+                await doctor.updateOne({ $pull: { followers: req.body.userId } });
+                await patient.updateOne({ $pull: { following: req.params.id } });
+                res.status(200).send("Doctor unfollowed");
+            } else {
+                res.status(400).send("You do not follow this doctor");
+            }
+        } catch (e) {
+            res.status(500).send(e);
+        }
+
+    }
+    else {
+        res.status(401).send("You can't unfollow yourself");
+    }
+})
+
 module.exports = router;
 
 
 // Get all doctors
 // Get a doctor
+// Follow a doctor
